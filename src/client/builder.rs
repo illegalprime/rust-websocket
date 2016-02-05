d6a5771d49f2ab556f7c24f6b7074c776a034170
Michael Eden <themichaeleden@gmail.com>
2016-02-05 17:08:09 -0500

    fleshed out prepare method for initializing streams

--- src/client/builder.rs
+++ src/client/builder.rs
@@ -2,14 +2,30 @@ extern crate url;
 extern crate openssl;
 
 use std::borrow::Borrow;
+use std::io::Error as IoError;
+use std::net::{
+    TcpStream,
+    Ipv6Addr,
+    Ipv4Addr,
+};
 
-use self::openssl::ssl::SslContext;
-use self::url::Url;
-use super::Client;
+use self::openssl::ssl::{
+    SslContext,
+    SslMethod,
+    SslStream,
+};
+use self::openssl::ssl::error::SslError;
+use self::url::{
+    Url,
+    Host,
+};
 use super::super::stream::WebSocketStream;
 use super::super::dataframe::DataFrame;
 use super::super::sender::Sender;
 use super::super::receiver::Receiver;
+
+pub type Client = super::Client<DataFrame, Sender<WebSocketStream>, Receiver<WebSocketStream>>;
+pub type Request = super::request::Request<WebSocketStream, WebSocketStream>;
 
 /// Build clients with a builder-style API
 pub struct ClientBuilder<'u, 'p, 'e, 's> {
@@ -56,7 +72,65 @@ impl<'u, 'p, 'e, 's> ClientBuilder<'u, 'p, 'e, 's> {
         self
     }
 
-    pub fn connect(&self) -> Client<DataFrame, Sender<WebSocketStream>, Receiver<WebSocketStream>> {
+    pub fn prepare(&self) -> Result<Request, ConnErr> {
+        // Get info about ports
+        let is_ssl = &self.url.scheme == "wss";
+        let port = if let Some(port) = self.url.port() {
+            port
+        } else {
+            if is_ssl {
+                443
+            } else {
+                80
+            }
+        };
+
+        // Make the TcpStream!
+        let stream = if let Some(host) = self.url.host() {
+            match *host {
+                Host::Domain(ref d) => TcpStream::connect((d as &str, port)),
+                Host::Ipv6(ip) => TcpStream::connect((ip, port)),
+                Host::Ipv4(ip) => TcpStream::connect((ip, port)),
+            }
+        } else {
+            return Err(ConnErr::NoHost);
+        };
+        let stream = match stream {
+            Ok(s) => s,
+            Err(e) => return Err(ConnErr::TcpConnect(e)),
+        };
+
+        // Make the WebSocketStream!
+        let stream = if is_ssl {
+            let ssl_stream = if let Some(context) = self.ssl_context {
+                SslStream::connect(context, stream)
+            } else {
+                let context = match SslContext::new(SslMethod::Tlsv1) {
+                    Ok(c) => c,
+                    Err(e) => return Err(ConnErr::MakingDefaultContext(e)),
+                };
+                SslStream::connect(&context, stream)
+            };
+            let ssl_stream = match ssl_stream {
+                Ok(s) => s,
+                Err(e) => return Err(ConnErr::SslConnect(e)),
+            };
+            WebSocketStream::Ssl(ssl_stream)
+        } else {
+            WebSocketStream::Tcp(stream)
+        };
+
         unimplemented!();
     }
+
+    pub fn connect(&self) -> Result<Client, ConnErr> {
+        unimplemented!();
+    }
+}
+
+pub enum ConnErr {
+    NoHost,
+    MakingDefaultContext(SslError),
+    SslConnect(SslError),
+    TcpConnect(IoError),
 }
